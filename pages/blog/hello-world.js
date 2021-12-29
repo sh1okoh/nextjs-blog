9a1eb667bb5010c3166721197ed08d9b732b9a69
sh1okoh <kooooo.0719task@gmail.com>
2021-12-29 17:06:26 +0900

    fix: さまざまな追加

--- pages/blog/hello-world.js
+++ pages/blog/hello-world.js
@@ -1,3 +1,15 @@
+import Image from 'next/image'
+ 
 export default function HelloWorld() {
-  return <h1>Hello World</h1>
+  return (
+  <div>
+    <h1>Hello World</h1>
+    <Image
+    src="/images/japan.jpg"
+    height={144}
+    width={144}
+    alt="japan"
+    />
+  </div>
+  )
 }
