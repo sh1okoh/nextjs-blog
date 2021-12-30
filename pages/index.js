0710b6ae56aadeb5444af1fe536c5addca0285ad
sh1okoh <kooooo.0719task@gmail.com>
2021-12-30 09:58:46 +0900

    feat: さまざまな追加

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <h1 class="title">
+      <h1 className='title'>
         Read {' '}
         <Link href="/posts/first-post">
           <a>this page!</a>
